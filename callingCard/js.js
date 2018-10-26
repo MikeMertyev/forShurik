9003549f21449a8df71ec4f013da40b2c2bfc7a5
MikeMertyev <mermiale@gmail.com>
2018-10-26 13:20:18 +0300

    remove comments

--- callingCard/js.js
+++ callingCard/js.js
@@ -1,43 +1,3 @@
-//let d = 100;
-//
-//
-//window.addEventListener("wheel", event => stickHeader(event));
-//
-//function stickHeader(event) {
-//    //console.log(window.pageYOffset);
-//    //if (this.scrollTop) {
-//    //document.querySelector('header').classList.add('sticky')}
-//    //}  else {document.querySelector('.header').classList.remove('.sticky')}
-//    //let scrolled = window.pageYOffset;
-//    //if (scrolled > scrolled1) {
-//    //    d = d - 2;
-//    //    console.log(d);
-//    //    document.querySelector('.header').style.height = `${d}vh`;
-//    //    scrolled1 = window.pageYOffset;
-//    //} else if (scrolled < scrolled1) {
-//    //    d = d + 2;
-//    //    document.querySelector('.header').style.height = `${d}vh`;
-//    //    scrolled1 = window.pageYOffset;
-//    //}
-//    //if (event.deltaY < 0) {
-//    //    //console.log('scrolling up');
-//    //    document.querySelector('header').classList.remove('sticky')
-//    //    //document.getElementById('status').innerHTML = 'scrolling up';
-//    //}
-//    if (event.deltaY > 0 && d >= 50) {
-//        d = d - 8;
-//        //console.log('scrolling down');
-//        document.querySelector('.header').style.height = `${d}vh`;
-//        //document.querySelector('.header').classList.add('sticky');
-//        //document.getElementById('status').innerHTML = 'scrolling down';
-//        console.log(d);
-//    } else if (event.deltaY < 0 && d < 100) {
-//            d = d + 8;
-//            document.querySelector('.header').style.height = `${d}vh`;
-//            console.log(d);
-//        //    scrolled1 = window.pageYOffset;
-//        }
-//}
 
 window.onscroll = function() {
 
@@ -54,8 +14,4 @@ window.onscroll = function() {
         //window.scrollBy(0, 10);
     }  else if (scrolled === 0 && document.querySelector('.sticky')){
         document.querySelector('header').classList.remove('sticky')}
-    //if (document.querySelector('.sticky') && scrolled ===0) {
-    //    console.log(scrolled);
-    //    window.scrollTo( 0, 1 )
-    //}
 };
